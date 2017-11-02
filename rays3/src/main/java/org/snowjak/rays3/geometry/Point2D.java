b8bf25f04e9e58aee15ef9eb9e348e897bd074fd
snowjak88 <sjnowak88@gmail.com>
2017-11-02 13:54:19 -0400

    Added simple 2-d Point class

---
+++ rays3/src/main/java/org/snowjak/rays3/geometry/Point2D.java
@@ -0,0 +1,34 @@
+package org.snowjak.rays3.geometry;
+
+/**
+ * Defines a 2-dimensional coordinate-pair.
+ * 
+ * @author snowjak88
+ */
+public class Point2D {
+
+	private double x, y;
+
+	/**
+	 * Create a new 2-d Point at (0,0)
+	 */
+	public Point2D() {
+		this(0d, 0d);
+	}
+
+	public Point2D(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+
+		return x;
+	}
+
+	public double getY() {
+
+		return y;
+	}
+
+}
