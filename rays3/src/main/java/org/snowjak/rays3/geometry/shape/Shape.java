72df40db6f2c3699d671c583c0d73e6638e0ae65
snowjak88 <sjnowak88@gmail.com>
2017-11-06 16:03:34 -0500

    Added placeholder interface for primitive "Shape"

---
+++ rays3/src/main/java/org/snowjak/rays3/geometry/shape/Shape.java
@@ -0,0 +1,12 @@
+package org.snowjak.rays3.geometry.shape;
+
+import org.snowjak.rays3.intersect.Interactable;
+
+/**
+ * Defines a 3-d shape procedurally.
+ * 
+ * @author snowjak88
+ */
+public interface Shape extends Interactable {
+
+}
