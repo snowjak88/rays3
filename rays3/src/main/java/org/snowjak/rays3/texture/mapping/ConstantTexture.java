5c4261c9793dc0356247e5baa1f3320989e71c75
snowjak88 <sjnowak88@gmail.com>
2017-11-07 14:52:02 -0500

    New ConstantTexture -- simplest texture there is

---
+++ rays3/src/main/java/org/snowjak/rays3/texture/mapping/ConstantTexture.java
@@ -0,0 +1,28 @@
+package org.snowjak.rays3.texture.mapping;
+
+import org.snowjak.rays3.geometry.shape.SurfaceDescriptor;
+import org.snowjak.rays3.spectrum.Spectrum;
+import org.snowjak.rays3.texture.Texture;
+
+/**
+ * A "constant" texture is simply one color, all the time.
+ * 
+ * @author snowjak88
+ */
+public class ConstantTexture extends Texture {
+
+	private Spectrum constant;
+
+	public ConstantTexture(Spectrum constant) {
+		super(new LinearTextureMapping());
+
+		this.constant = constant;
+	}
+
+	@Override
+	public Spectrum evaluate(SurfaceDescriptor surface) {
+
+		return constant;
+	}
+
+}
