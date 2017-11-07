f4749b2eeb087090ce01932a01a60e9798d55357
snowjak88 <sjnowak88@gmail.com>
2017-11-07 16:12:55 -0500

    Filled in the details on Sample

--- rays3/src/main/java/org/snowjak/rays3/sample/Sample.java
+++ rays3/src/main/java/org/snowjak/rays3/sample/Sample.java
@@ -1,9 +1,16 @@
 package org.snowjak.rays3.sample;
 
+import java.util.Random;
+import java.util.function.Supplier;
+
+import org.snowjak.rays3.geometry.Point2D;
+import org.snowjak.rays3.spectrum.Spectrum;
+
 /**
  * Represents a single sample from the world.
  * <p>
- * A Sample is composed of many different "sub-samples", which can include dimensions such as:
+ * A Sample is composed of many different "sub-samples", which can include
+ * dimensions such as:
  * <ul>
  * <li>Location on the image-plane (x,y)</li>
  * <li>Location on the camera-lens (u,v)</li>
@@ -11,13 +18,211 @@ package org.snowjak.rays3.sample;
  * <li>Spectrum to sample (nm)</li>
  * </ul>
  * </p>
+ * 
  * @author snowjak88
- *
  */
 public class Sample {
 
-	public Sample() {
-		// TODO Auto-generated constructor stub
+	private Sampler				sampler;
+
+	private double				imageX, imageY;
+	private double				lensU, lensV;
+	private double				t;
+	private Spectrum			wavelength;
+
+	private Supplier<Double>	singleSampleSupplier;
+	private Supplier<Point2D>	twinSampleSupplier;
+
+	/**
+	 * Construct a new Sample.
+	 * <p>
+	 * The lens-location to be sampled, ( <code>lensU</code>,
+	 * <code>lensV</code>), is given a default value of (0.5, 0.5).
+	 * </p>
+	 * <p>
+	 * The time of exposure, <code>t</code>, is given a default value of 0.5
+	 * (i.e., at the camera shutter's halfway, wide-open point).
+	 * </p>
+	 * <p>
+	 * The wavelength to be sampled is given a default value of
+	 * <code>null</code> (i.e., no particular wavelength).
+	 * </p>
+	 * <p>
+	 * For the embedded {@link Supplier}s, default implementations are provided
+	 * (relying on Java's built-in psuedo-random number generation).
+	 * </p>
+	 * 
+	 * @param sampler
+	 * @param imageX
+	 * @param imageY
+	 */
+	public Sample(Sampler sampler, double imageX, double imageY) {
+		this(sampler, imageX, imageY, 0.5, 0.5);
+	}
+
+	/**
+	 * Construct a new Sample.
+	 * <p>
+	 * The time of exposure, <code>t</code>, is given a default value of 0.5
+	 * (i.e., at the camera shutter's halfway, wide-open point).
+	 * </p>
+	 * <p>
+	 * The wavelength to be sampled is given a default value of
+	 * <code>null</code> (i.e., no particular wavelength).
+	 * </p>
+	 * <p>
+	 * For the embedded {@link Supplier}s, default implementations are provided
+	 * (relying on Java's built-in psuedo-random number generation).
+	 * </p>
+	 * 
+	 * @param sampler
+	 * @param imageX
+	 * @param imageY
+	 * @param lensU
+	 * @param lensV
+	 */
+	public Sample(Sampler sampler, double imageX, double imageY, double lensU, double lensV) {
+		this(sampler, imageX, imageY, lensU, lensV, 0.5d, null);
+	}
+
+	/**
+	 * Construct a new Sample.
+	 * <p>
+	 * For the embedded {@link Supplier}s, default implementations are provided
+	 * (relying on Java's built-in psuedo-random number generation).
+	 * </p>
+	 * 
+	 * @param sampler
+	 * @param imageX
+	 * @param imageY
+	 * @param lensU
+	 * @param lensV
+	 * @param t
+	 * @param wavelength
+	 */
+	public Sample(Sampler sampler, double imageX, double imageY, double lensU, double lensV, double t,
+			Spectrum wavelength) {
+		this(sampler, imageX, imageY, lensU, lensV, t, wavelength, new Supplier<Double>() {
+
+			private Random rnd = new Random(System.currentTimeMillis());
+
+			@Override
+			public Double get() {
+
+				return rnd.nextDouble();
+			}
+
+		}, new Supplier<Point2D>() {
+
+			private Random rnd = new Random(System.currentTimeMillis());
+
+			@Override
+			public Point2D get() {
+
+				return new Point2D(rnd.nextDouble(), rnd.nextDouble());
+			}
+
+		});
+	}
+
+	/**
+	 * Construct a new Sample.
+	 * 
+	 * @param sampler
+	 * @param imageX
+	 * @param imageY
+	 * @param lensU
+	 * @param lensV
+	 * @param t
+	 * @param wavelength
+	 * @param singleSampleSupplier
+	 * @param twinSampleSupplier
+	 */
+	public Sample(Sampler sampler, double imageX, double imageY, double lensU, double lensV, double t,
+			Spectrum wavelength, Supplier<Double> singleSampleSupplier, Supplier<Point2D> twinSampleSupplier) {
+
+		this.sampler = sampler;
+		this.imageX = imageX;
+		this.imageY = imageY;
+		this.lensU = lensU;
+		this.lensV = lensV;
+		this.t = t;
+		this.wavelength = wavelength;
+		this.singleSampleSupplier = singleSampleSupplier;
+		this.twinSampleSupplier = twinSampleSupplier;
+	}
+
+	/**
+	 * @return the {@link Sampler} which generated this Sample
+	 */
+	public Sampler getSampler() {
+
+		return sampler;
+	}
+
+	/**
+	 * @return the image X coordinate to be sampled
+	 */
+	public double getImageX() {
+
+		return imageX;
+	}
+
+	/**
+	 * @return the image Y coordinate to be sampled
+	 */
+	public double getImageY() {
+
+		return imageY;
+	}
+
+	/**
+	 * @return the lens U coordinate to be sampled
+	 */
+	public double getLensU() {
+
+		return lensU;
+	}
+
+	/**
+	 * @return the lens V coordinate to be sampled
+	 */
+	public double getLensV() {
+
+		return lensV;
+	}
+
+	/**
+	 * @return the camera-shutter time to be sampled
+	 */
+	public double getT() {
+
+		return t;
+	}
+
+	/**
+	 * @return the wavelength to be sampled, or <code>null</code> if no
+	 *         wavelength in particular
+	 */
+	public Spectrum getWavelength() {
+
+		return wavelength;
+	}
+
+	/**
+	 * @return an additional single (i.e., <code>double</code>) sample
+	 */
+	public double getAdditionalSingleSample() {
+
+		return singleSampleSupplier.get();
+	}
+
+	/**
+	 * @return an additional twin (i.e., {@link Point2D}) sample
+	 */
+	public Point2D getAdditionalTwinSample() {
+
+		return twinSampleSupplier.get();
 	}
 
 }
