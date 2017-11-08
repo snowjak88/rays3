5753e3765d6678d5dc3e36183c32b588ae39263a
snowjak88 <sjnowak88@gmail.com>
2017-11-08 13:11:00 -0500

    Remember to shut down Global executor!

--- rays3/src/main/java/org/snowjak/rays3/Main.java
+++ rays3/src/main/java/org/snowjak/rays3/Main.java
@@ -1,11 +1,14 @@
 package org.snowjak.rays3;
 
-
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("rays3 main() stub");
+
+		//
+		// Remember to shut down the global executor!
+		Global.EXECUTOR.shutdown();
 	}
 
 }
