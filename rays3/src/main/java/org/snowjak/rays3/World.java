75e779e38664fe76471dd739f0af6632cc64ce07
snowjak88 <sjnowak88@gmail.com>
2017-11-14 10:12:34 -0500

    Added collection of Lights to World

--- rays3/src/main/java/org/snowjak/rays3/World.java
+++ rays3/src/main/java/org/snowjak/rays3/World.java
@@ -2,10 +2,9 @@ package org.snowjak.rays3;
 
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.stream.Collectors;
 
-import org.snowjak.rays3.bxdf.BDSF;
 import org.snowjak.rays3.geometry.shape.Primitive;
+import org.snowjak.rays3.light.Light;
 
 /**
  * Describes the world as a whole.
@@ -15,31 +14,15 @@ import org.snowjak.rays3.geometry.shape.Primitive;
 public class World {
 
 	private Collection<Primitive>	primitives	= new LinkedList<>();
-	private Collection<Primitive>	lights		= null;
+	private Collection<Light>		lights		= new LinkedList<>();
 
 	public Collection<Primitive> getPrimitives() {
 
-		synchronized (this) {
-			this.lights = null;
-
-			return primitives;
-		}
+		return primitives;
 	}
 
-	/**
-	 * Compute the list of those {@link Primitive}s that have configured their
-	 * {@link BDSF}s to be "emissive" -- i.e., such that their
-	 * <code>{@link BDSF#hasEmissiveRadiance()} == true</code>.
-	 */
-	public Collection<Primitive> getLights() {
+	public Collection<Light> getLights() {
 
-		synchronized (this) {
-			if (lights != null)
-				return lights;
-
-			lights = getPrimitives().stream().filter(p -> p.getBdsf().hasEmissiveRadiance()).collect(
-					Collectors.toCollection(LinkedList::new));
-			return lights;
-		}
+		return lights;
 	}
 }
