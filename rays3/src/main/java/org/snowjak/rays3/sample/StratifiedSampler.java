a039c79761c1c590cc38cd6c5e6ae120c37fe96d
snowjak88 <sjnowak88@gmail.com>
2017-11-16 11:24:58 -0500

    Added Stratfied Sampler

---
+++ rays3/src/main/java/org/snowjak/rays3/sample/StratifiedSampler.java
@@ -0,0 +1,228 @@
+package org.snowjak.rays3.sample;
+
+import static org.apache.commons.math3.util.FastMath.*;
+
+import java.util.function.Supplier;
+
+import org.snowjak.rays3.Global;
+import org.snowjak.rays3.film.Film;
+import org.snowjak.rays3.geometry.Point2D;
+import org.snowjak.rays3.spectrum.Spectrum;
+
+/**
+ * Implements "stratified" sampling.
+ * <p>
+ * Each pixel is sampled <code>n</code> times, where
+ * <code>n = i<sup>2</sup></code> for a positive integer <code>i</code>. Each
+ * pixel's continuous coordinate-space is divided evenly into <code>n</code>
+ * cells, constituting a <code>sqrt(n) x sqrt(n)</code> grid. Within each cell,
+ * a point is selected as that sample's image-plane coordinate.
+ * </p>
+ * <p>
+ * This technique is repeated when selecting sample-points on the lens.
+ * </p>
+ * 
+ * @author snowjak88
+ */
+public class StratifiedSampler extends Sampler {
+
+	private int					currFilmX, currFilmY, currPixelSample;
+
+	private Supplier<Point2D>	imageSamples, lensSamples;
+	private Supplier<Double>	timeSamples;
+	private final int			gridSideSize;
+
+	/**
+	 * Create a new StratifiedSampler.
+	 * <p>
+	 * <strong>Note</strong> that <code>samplesPerPixel</code> must be a power
+	 * of 2. If necessary, this number is <strong>rounded up</strong>.
+	 * </p>
+	 * 
+	 * @param filmSizeX
+	 * @param filmSizeY
+	 * @param samplesPerPixel
+	 */
+	public StratifiedSampler(int filmSizeX, int filmSizeY, int samplesPerPixel) {
+		super(0, 0, filmSizeX - 1, filmSizeY - 1, (int) pow(ceil(sqrt((double) samplesPerPixel)), 2));
+
+		currFilmX = 0;
+		currFilmY = 0;
+		currPixelSample = -1;
+
+		gridSideSize = (int) ceil(sqrt((double) samplesPerPixel));
+
+		imageSamples = new Stratified2DSupplier(gridSideSize);
+		lensSamples = new Stratified2DSupplier(gridSideSize);
+		timeSamples = new Stratified1DSupplier(gridSideSize);
+	}
+
+	@Override
+	public Sample getNextSample() {
+
+		currPixelSample++;
+		if (currPixelSample >= getSamplesPerPixel()) {
+
+			currPixelSample = 0;
+			currFilmY++;
+
+			if (currFilmY > getMaxFilmY()) {
+				currFilmY = getMinFilmY();
+				currFilmX++;
+
+				if (currFilmX > getMaxFilmX())
+					return null;
+			}
+		}
+
+		final Point2D imageSample = imageSamples.get();
+		final Point2D lensSample = lensSamples.get();
+		final double timeSample = timeSamples.get();
+
+		final double imageU = mapXToU(imageSample.getX() + (double) currFilmX,
+				Film.convertDiscreteToContinuous(getMinFilmX()), Film.convertDiscreteToContinuous(getMaxFilmX()));
+		final double imageV = mapXToU(imageSample.getY() + (double) currFilmY,
+				Film.convertDiscreteToContinuous(getMinFilmY()), Film.convertDiscreteToContinuous(getMaxFilmY()));
+
+		return new Sample(this, imageSample.getX() + (double) currFilmX, imageSample.getY() + (double) currFilmY,
+				imageU, imageV, lensSample.getX(), lensSample.getY(), timeSample, null,
+				new Supplier<Supplier<Double>>() {
+
+					@Override
+					public Supplier<Double> get() {
+
+						return new Stratified1DSupplier(gridSideSize);
+					}
+
+				}, new Supplier<Supplier<Point2D>>() {
+
+					@Override
+					public Supplier<Point2D> get() {
+
+						return new Stratified2DSupplier(gridSideSize);
+					}
+
+				});
+	}
+
+	@Override
+	public boolean isSampleAcceptable(Sample sample, Spectrum result) {
+
+		// All samples are always considered to be acceptable -- i.e., we have
+		// no criterion for rejecting Sample results.
+		return true;
+	}
+
+	/**
+	 * Supplies stratified <code>double</code> samples on the interval
+	 * <code>[0..1]</code>, from smallest to largest. When the current set of
+	 * stratified samples is completely scanned, this Supplier automatically
+	 * compiles a new set of samples.
+	 * 
+	 * @author snowjak88
+	 */
+	public static class Stratified1DSupplier implements Supplier<Double> {
+
+		private double[]	stratum;
+		private int			currentElement;
+
+		public Stratified1DSupplier(int length) {
+			this.stratum = generate1DStratum(length);
+			this.currentElement = -1;
+		}
+
+		@Override
+		public Double get() {
+
+			currentElement++;
+
+			if (currentElement >= stratum.length) {
+				stratum = generate1DStratum(stratum.length);
+				currentElement = 0;
+			}
+
+			return stratum[currentElement];
+		}
+
+	}
+
+	/**
+	 * Supplies stratified {@link Point2D} samples on the interval
+	 * <code>[(0,0), (1,1)]</code>, by columns first. When the current set of
+	 * stratified samples is completely scanned, this Supplier automatically
+	 * compiles a new set of samples.
+	 * 
+	 * @author snowjak88
+	 */
+	public static class Stratified2DSupplier implements Supplier<Point2D> {
+
+		private Point2D[][]	stratum;
+		private int			currentI, currentJ;
+
+		public Stratified2DSupplier(int sideLength) {
+			this.stratum = generate2DStratum(sideLength);
+			this.currentI = 0;
+			this.currentJ = -1;
+		}
+
+		@Override
+		public Point2D get() {
+
+			this.currentJ++;
+
+			if (currentJ >= stratum[currentI].length) {
+				currentJ = 0;
+				currentI++;
+
+				if (currentI >= stratum.length) {
+					currentI = 0;
+					currentJ = 0;
+					stratum = generate2DStratum(stratum.length);
+				}
+			}
+
+			return stratum[currentI][currentJ];
+		}
+
+	}
+
+	public static double[] generate1DStratum(int length) {
+
+		final double[] results = new double[length];
+
+		final double sampleWidth = 1d / (double) length;
+		for (int i = 0; i < length; i++) {
+
+			final double jitter = Global.RND.nextDouble();
+			results[i] = ( (double) i * sampleWidth ) + ( jitter * sampleWidth );
+
+		}
+
+		return results;
+	}
+
+	public static Point2D[][] generate2DStratum(int sideLength) {
+
+		final Point2D[][] cells = new Point2D[sideLength][sideLength];
+		final double cellLengthX = 1d / ( (double) sideLength );
+
+		for (int i = 0; i < cells.length; i++) {
+
+			final double cellLengthY = 1d / ( (double) cells[i].length );
+
+			for (int j = 0; j < cells[0].length; j++) {
+
+				final double jitterX = Global.RND.nextDouble();
+				final double jitterY = Global.RND.nextDouble();
+
+				final double x = ( (double) i * cellLengthX ) + ( jitterX * cellLengthX );
+				final double y = ( (double) j * cellLengthY ) + ( jitterY * cellLengthY );
+
+				cells[i][j] = new Point2D(x, y);
+			}
+		}
+
+		return cells;
+	}
+
+}
---
+++ rays3/src/test/java/org/snowjak/rays3/sample/StratifiedSamplerTest.java
@@ -0,0 +1,114 @@
+package org.snowjak.rays3.sample;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.snowjak.rays3.film.Film;
+import org.snowjak.rays3.geometry.Point2D;
+
+public class StratifiedSamplerTest {
+
+	private StratifiedSampler	sampler;
+	private short[][]			film;
+
+	@Before
+	public void setUp() throws Exception {
+
+		this.sampler = new StratifiedSampler(64, 64, 4);
+		this.film = new short[64][64];
+	}
+
+	public void testPixelSampleCount() {
+
+		StratifiedSampler sampler = new StratifiedSampler(4, 4, 10);
+		assertEquals(16, sampler.getSamplesPerPixel());
+	}
+
+	@Test
+	public void testGetNextSample() {
+
+		for (int i = sampler.getMinFilmX(); i <= sampler.getMaxFilmX(); i++)
+			for (int j = sampler.getMinFilmY(); j <= sampler.getMaxFilmY(); j++) {
+				film[i][j] = 0;
+			}
+
+		Sample sample = sampler.getNextSample();
+		assertNotNull("First sample was null!", sample);
+
+		while (sample != null) {
+
+			final double imageX = sample.getImageX();
+			final double imageY = sample.getImageY();
+			final int filmX = Film.convertContinuousToDiscrete(imageX);
+			final int filmY = Film.convertContinuousToDiscrete(imageY);
+
+			assertTrue("Film-X (" + filmX + ") out of bounds!", ( filmX >= 0 ) && ( filmX < film.length ));
+			assertTrue("Film-Y (" + filmY + ") out of bounds!", ( filmY >= 0 ) && ( filmY < film[0].length ));
+
+			film[filmX][filmY]++;
+
+			sample = sampler.getNextSample();
+		}
+
+		for (int i = sampler.getMinFilmX(); i <= sampler.getMaxFilmX(); i++)
+			for (int j = sampler.getMinFilmY(); j <= sampler.getMaxFilmY(); j++) {
+				assertEquals("Film-location at [" + i + "," + j + "] not visited expected number of times!", 4,
+						film[i][j]);
+			}
+	}
+
+	@Test
+	public void testIsSampleAcceptable() {
+
+		assertTrue("Expect this to always be true!", sampler.isSampleAcceptable(null, null));
+	}
+
+	@Test
+	public void testGenerate1DStratum() {
+
+		double[] stratum = StratifiedSampler.generate1DStratum(8);
+
+		assertEquals("Stratum length not as expected!", 8, stratum.length);
+
+		for (int i = 0; i < 8; i++) {
+
+			final double value = stratum[i];
+			final double minValue = (double) i / 8d, maxValue = (double) i / 8d + 1d;
+			assertTrue(
+					"Stratum element (" + Double.toString(value) + ") is not within expected bounds ("
+							+ Double.toString(minValue) + ", " + Double.toString(maxValue) + ").",
+					( value >= minValue ) && ( value <= maxValue ));
+		}
+	}
+
+	@Test
+	public void testGenerate2DStratum() {
+
+		Point2D[][] stratum = StratifiedSampler.generate2DStratum(8);
+
+		assertEquals("Stratum length not as expected!", 8, stratum.length);
+		assertEquals("Stratum[0] length not as expected!", 8, stratum[0].length);
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+
+				final Point2D value = stratum[i][j];
+				final double minValueX = (double) i / 8d, maxValueX = (double) i / 8d + 1d;
+				final double minValueY = (double) j / 8d, maxValueY = (double) j / 8d + 1d;
+
+				assertTrue(
+						"Stratum element [" + i + "," + j + "].X = (" + Double.toString(value.getX())
+								+ ") is not within expected bounds (" + Double.toString(minValueX) + ", "
+								+ Double.toString(maxValueY) + ").",
+						( value.getX() >= minValueX ) && ( value.getX() <= maxValueX ));
+				assertTrue(
+						"Stratum element [" + i + "," + j + "].Y = (" + Double.toString(value.getY())
+								+ ") is not within expected bounds (" + Double.toString(minValueY) + ", "
+								+ Double.toString(maxValueY) + ").",
+						( value.getY() >= minValueY ) && ( value.getY() <= maxValueY ));
+			}
+		}
+	}
+
+}
